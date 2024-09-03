d68166b077c26cd391c9dc644a2812a9562de57e
Carlos <digiampaolo@outlook.com>
2024-09-03 09:33:32 -0400

    ejercicio terminado

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,38 +6,57 @@ window.onload = () => {
 let card_head = document.querySelector('#header');
 let card_footer = document.querySelector('#footer'); 
 let boton_cargar_carta = document.querySelector('.button_new_card');
+let numberSelection = document.querySelector('#number');
+let numberColor = document.querySelector('.number');
 
 boton_cargar_carta.addEventListener('click', () => {
   generarCartaAleatoria();
 });
+
+let card_arr =  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+function generarNumeroAleatorio () {
+  // funcion que retorna un numero aleatorio.
+  let card_numberValue = Math.floor(Math.random() * card_arr.length);
+  if (card_numberValue <= card_arr.length && card_numberValue>= 0) {
+  return  respuesta = card_arr[card_numberValue] 
+}};
+
 
 function generarCartaAleatoria() {
   //alert('Generando carta...');
   card_head.className = '';
   card_footer.className = '';
   let pinta = generarPintaAleatoria();
+  numberSelection.textContent = generarNumeroAleatorio();
   card_head.className = pinta;
   card_footer.className = pinta + ' inverse';
-}
 
-function generarPintaAleatoria() {
-  let num_aleatorio_pinta = Math.round(Math.random() * 4);
-  switch (num_aleatorio_pinta) {
-    case 1:
-      pinta = 'heart';
-      break;
+  function generarPintaAleatoria() {
+    let num_aleatorio_pinta = Math.floor(Math.random() * 4)+1 ;
+    let pinta; 
   
-    case 2:
-      pinta = 'diamond';
-      break;
-  
-    case 3:
-      pinta = 'spade';
-      break;
-  
-    case 4:
-      pinta = 'club';
-      break;
-  }
-  return pinta;
-}
+    switch (num_aleatorio_pinta) {
+      case 1:
+        pinta = 'heart';
+        numberColor.style.color="red";
+        break;
+      case 2:
+        pinta = 'diamond';
+        numberColor.style.color="red";
+        break;
+      case 3:
+        pinta = 'spade';
+        numberColor.style.color="black";
+        break;
+      case 4:
+        pinta = 'club';
+        numberColor.style.color="black";
+        break;
+      default:
+        pinta = 'unknown';
+        break;
+    }
+    
+    return pinta;
+  }};
